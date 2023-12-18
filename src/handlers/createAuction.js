cc495bf47b9b7b046fd2394339ce4121e75d3666
zahid-nazir <rzual4u@gmail.com>
2023-12-18 18:10:04 +0530

    Adding the source code for lab work

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,28 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+  };
+
+  await dynamoDB.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+};
+
+
